52371203227d1858b14fc8da96313ba6f19a4abf
WoutDemeyere <wout@unit6.be>
2026-02-01 16:52:28 +0100

    fixed router

--- src/router.tsx
+++ src/router.tsx
@@ -50,6 +50,7 @@ const routeTree = rootRoute.addChildren([
 
 export const router = createRouter({
   routeTree,
+  basepath: import.meta.env.BASE_URL.replace(/\/$/, ''),
   defaultPreload: 'intent'
 })
 
